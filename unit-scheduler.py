d8b3983a35e026ff4260e7597928ef2b231e6ae2
tzorca <tzorca@users.noreply.github.com>
2015-01-27 22:55:14 -0500

    Added datetime parsing for start date and due date.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -1,3 +1,16 @@
+from datetime import datetime
+
 units = raw_input("Enter the number of work units in the project:")
-start_date = raw_input("(Optional) Enter the start date for the project:")
-due_date = raw_input("Enter the due date for the project:")
+start_date_str = raw_input("(Optional) Enter the start date for the project (yyyy/mm/dd):")
+due_date_str = raw_input("Enter the due date for the project (yyyy/mm/dd):")
+
+if (start_date_str):
+    start_date = datetime.strptime(start_date_str, "%Y/%m/%d")
+else:
+    start_date = datetime.now()
+
+due_date = datetime.strptime(due_date_str, "%Y/%m/%d")
+
+print(start_date.strftime("%B %d, %Y") + " to " + due_date.strftime("%B %d, %Y"))
+
+raw_input()
