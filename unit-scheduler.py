81ed492d82d2de081faf4faa3367d6ce9092f7da
tzorca <tzorca@users.noreply.github.com>
2015-01-27 22:40:05 -0500

    Requested optional start date.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -1,3 +1,3 @@
 units = raw_input("Enter the number of work units in the project:")
+start_date = raw_input("(Optional) Enter the start date for the project:")
 due_date = raw_input("Enter the due date for the project:")
-
