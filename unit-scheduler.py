9d610ab15bd49b503f35b215de7cc655c428f5ea
tzorca <tzorca@users.noreply.github.com>
2015-01-27 22:57:31 -0500

    Improved readability of input request messages.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 
-units = raw_input("Enter the number of work units in the project:")
-start_date_str = raw_input("(Optional) Enter the start date for the project (yyyy/mm/dd):")
-due_date_str = raw_input("Enter the due date for the project (yyyy/mm/dd):")
+units = raw_input("Enter the number of work units: ")
+start_date_str = raw_input("(Optional) Enter the start date (yyyy/mm/dd): ")
+due_date_str = raw_input("Enter the due date (yyyy/mm/dd): ")
 
 if (start_date_str):
     start_date = datetime.strptime(start_date_str, "%Y/%m/%d")
