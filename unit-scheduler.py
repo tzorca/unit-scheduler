05525cf0fec6c638fc09d5c8d752a2204f259de7
tzorca <tzorca@users.noreply.github.com>
2015-01-27 23:13:46 -0500

    Made input request messages more succinct.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -2,9 +2,9 @@ from datetime import datetime
 
 print("\n-- Unit Scheduler for Small Projects --\n")
 
-units = raw_input("Enter the number of work units: ")
-start_date_str = raw_input("(Optional) Enter the start date (yyyy-mm-dd): ")
-due_date_str = raw_input("Enter the due date (yyyy-mm-dd): ")
+units = raw_input("Enter number of work units: ")
+start_date_str = raw_input("(Optional) Enter start date (yyyy-mm-dd): ")
+due_date_str = raw_input("Enter due date (yyyy-mm-dd): ")
 
 if (start_date_str):
     start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
