480bea480836f7fad150a3f3725fd8645872095c
tzorca <tzorca@users.noreply.github.com>
2015-01-27 23:12:28 -0500

    Tweaked date input format.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -3,15 +3,15 @@ from datetime import datetime
 print("\n-- Unit Scheduler for Small Projects --\n")
 
 units = raw_input("Enter the number of work units: ")
-start_date_str = raw_input("(Optional) Enter the start date (yyyy/mm/dd): ")
-due_date_str = raw_input("Enter the due date (yyyy/mm/dd): ")
+start_date_str = raw_input("(Optional) Enter the start date (yyyy-mm-dd): ")
+due_date_str = raw_input("Enter the due date (yyyy-mm-dd): ")
 
 if (start_date_str):
-    start_date = datetime.strptime(start_date_str, "%Y/%m/%d")
+    start_date = datetime.strptime(start_date_str, "%Y-%m-%d")
 else:
     start_date = datetime.now()
 
-due_date = datetime.strptime(due_date_str, "%Y/%m/%d")
+due_date = datetime.strptime(due_date_str, "%Y-%m-%d")
 
 # Testing
 print(start_date.strftime("%B %d, %Y") + " to " + due_date.strftime("%B %d, %Y"))
