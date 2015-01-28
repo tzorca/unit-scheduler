9dcec33446729e7bb38e1c7482813a85de48e5cf
tzorca <tzorca@users.noreply.github.com>
2015-01-27 23:28:36 -0500

    Calculated and printed project unit schedule.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 print("\n-- Unit Scheduler for Small Projects --\n")
 
@@ -24,6 +24,15 @@ print(str(days_between_dates) + " days from start date to due date")
 units_per_day = float(units) / days_between_dates
 
 # Testing
-print("%.2f units per day" % (units_per_day))
+print("%.1f units per day" % (units_per_day))
+
+for day in range(1, days_between_dates+1):
+    current_date = start_date + timedelta(days=day)
+    current_date_str = current_date.strftime("%Y-%m-%d")
+
+    units_low = units_per_day * (day-1)
+    units_high = units_per_day * day
+
+    print("%s Units %.1f - %.1f" % (current_date_str, units_low, units_high))
 
 raw_input()
