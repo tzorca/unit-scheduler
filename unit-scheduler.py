50a3327a1585612d08d4185b5a31d2c9c2ef9bb4
tzorca <tzorca@users.noreply.github.com>
2015-01-27 23:03:48 -0500

    Calculated days between start date and due date.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 print("\n-- Unit Scheduler for Small Projects --\n")
+
 units = raw_input("Enter the number of work units: ")
 start_date_str = raw_input("(Optional) Enter the start date (yyyy/mm/dd): ")
 due_date_str = raw_input("Enter the due date (yyyy/mm/dd): ")
@@ -12,6 +13,12 @@ else:
 
 due_date = datetime.strptime(due_date_str, "%Y/%m/%d")
 
+# Testing
 print(start_date.strftime("%B %d, %Y") + " to " + due_date.strftime("%B %d, %Y"))
+
+days_between_dates = (due_date - start_date).days
+
+# Testing
+print(str(days_between_dates) + " days from start date to due date")
 
 raw_input()
