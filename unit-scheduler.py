f485de8eb24a94065c91415de541f659767759b0
tzorca <tzorca@users.noreply.github.com>
2015-01-27 23:10:27 -0500

    Calculated units per day.

--- unit-scheduler.py
+++ unit-scheduler.py
@@ -21,4 +21,9 @@ days_between_dates = (due_date - start_date).days
 # Testing
 print(str(days_between_dates) + " days from start date to due date")
 
+units_per_day = days_between_dates / float(units)
+
+# Testing
+print("%.2f units per day" % (units_per_day))
+
 raw_input()
